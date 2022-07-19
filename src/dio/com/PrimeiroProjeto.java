ff1eede9bef9ee59235ffddab2f7d32ed926d96f
Alyne <alyne_evelyn@hotmail.com>
2022-07-19 16:40:22 -0300

    primeiro commit do eclipse

---
+++ src/dio/com/PrimeiroProjeto.java
@@ -0,0 +1,20 @@
+package dio.com;
+
+import com.dio.manual.Gato;
+
+public class PrimeiroProjeto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Livros livros = new Livros();
+		Gato gato = new Gato("Dean", "Gray", 10);
+		
+		
+		System.out.println(livros);
+		System.out.println(gato);
+
+	}
+}
+
+class Livros {
+}
