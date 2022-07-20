6afa108be32c03eefb307f20e2d133c7cd1081ff
Alyne <alyne_evelyn@hotmail.com>
2022-07-20 17:44:03 -0300

    exercises about methods

---
+++ src/MethodsExercises/Main.java
@@ -0,0 +1,14 @@
+package MethodsExercises;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Calculator Exercise");
+		calculator.sum(3, 6);
+		calculator.subtraction(9, 1.5);
+		calculator.multiplication(7, 8);
+		calculator.division(5, 2.5);
+	}
+
+}
---
+++ src/MethodsExercises/calculator.java
@@ -0,0 +1,21 @@
+package MethodsExercises;
+
+public class calculator {
+
+	public static void sum(double n1, double n2) {
+		double result = n1 + n2;
+		System.out.println("The sum of " + n1 + " plus " + n2 + " is: " + result);
+	}
+	public static void subtraction(double n1, double n2) {
+		double result = n1 - n2;
+		System.out.println("Subtracting " + n1 + " minus " + n2 + " is: " + result);
+	}
+	public static void multiplication(double n1, double n2) {
+		double result = n1 * n2;
+		System.out.println("Multiplying " + n1 + " by " + n2 + " is: " + result);
+	}
+	public static void division(double n1, double n2) {
+		double result = n1 / n2;
+		System.out.println("The division of " + n1 + " by " + n2 + " is: " + result);
+	}
+}
